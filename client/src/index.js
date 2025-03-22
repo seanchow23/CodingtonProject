b6ad86b6773cc065861b7b9cfb60c41f0747d897
Denny Lin <denny.lin@stonybrook.edu>
2025-03-22 15:08:59 -0400

    moved styles.css to stylesheets

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import Home from './components/home'
 import reportWebVitals from './reportWebVitals';
-import './styles.css';
+import './stylesheets/styles.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
