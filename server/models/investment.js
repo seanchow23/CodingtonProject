f21edd74da464ca0c1a75d23357b163b35925902
Denny Lin <denny.lin@stonybrook.edu>
2025-04-14 15:17:39 -0400

    model adjustments (investment, reblanace, scenario)

--- server/models/investment.js
+++ server/models/investment.js
@@ -5,7 +5,7 @@ const InvestmentType = require('./investmentType');
 const investmentSchema = new mongoose.Schema({
     investmentType: { type: mongoose.Schema.Types.ObjectId, ref: 'InvestmentType', required: true },
     value: { type: Number, required: true },
-    basValue: { type: Number, required: true },
+    baseValue: { type: Number, required: true },
     taxStatus: { type: String, enum: ['non-retirement', 'pre-tax retirement', 'after-tax retirement'], required: true }
 });
 
