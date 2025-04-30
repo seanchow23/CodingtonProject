ad86700b29ff85a81260588ea26950daeb98ecdc
DL115 <143527544+DL115@users.noreply.github.com>
2025-04-30 00:36:50 -0400

    Reolved Overwritten code

--- client/src/components/create_scenario.js
+++ client/src/components/create_scenario.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import InputField from "./input_field";
-import { createScenario } from "../api/scenarioApi"; // Import the API function
+import { createScenario } from "../api/scenarioApi";
 import { getCurrentUser } from "../api/userApi";
 import { createInvestmentType } from "../api/investmentTypeApi";
 import * as investmentApi from "../api/investmentApi";
@@ -23,6 +23,7 @@ export default function CreateScenario({ scenarios }) {
     sharing: "",
     financialGoal: "",
     state: "",
+    random: [0, 77, 15, 0, 2.5, 1.9],
   });
 
   const [error, setError] = useState("");
@@ -64,6 +65,23 @@ export default function CreateScenario({ scenarios }) {
     setFormData({ ...formData, married: e.target.value });
   };
 
+  const handleRandom = (e) => {
+    const { name, value, checked } = e.target;
+    if (name === "random_life") {
+      setFormData({ ...formData, random: [checked ? 1 : 0, formData.random[1], formData.random[2], formData.random[3], formData.random[4], formData.random[5]] });
+    } else if (name === "mean") {
+      setFormData({ ...formData, random: [formData.random[0], Number(value), formData.random[2], formData.random[3], formData.random[4], formData.random[5]] });
+    } else if (name === "sd") {
+      setFormData({ ...formData, random: [formData.random[0], formData.random[1], Number(value), formData.random[3], formData.random[4], formData.random[5]] });
+    } else if (name === "random_inflation") {
+      setFormData({ ...formData, random: [formData.random[0], formData.random[1], formData.random[2], checked ? 1 : 0, formData.random[4], formData.random[5]] });
+    } else if (name === "mean_inflation") {
+      setFormData({ ...formData, random: [formData.random[0], formData.random[1], formData.random[2], formData.random[3], Number(value), formData.random[5]] });
+    } else if (name === "sd_inflation") {
+      setFormData({ ...formData, random: [formData.random[0], formData.random[1], formData.random[2], formData.random[3], formData.random[4], Number(value)] });
+    }
+  }
+
   const addScenario = (newScenario) => {
     scenarios.push(newScenario);
     navigate('/');
@@ -71,15 +89,12 @@ export default function CreateScenario({ scenarios }) {
 
   const submit = async (e) => {
     e.preventDefault();
-  
-    console.log("DEBUG: Form is being submitted!");
-  
     const check = Object.keys(formData).find((key) => formData[key] < 0);
     if (check) {
       setError(`The ${check} field cannot have a negative value.`);
       return;
     }
-  
+    
     const newInvestmentType = {
       name: "Cash",
       description: "",
@@ -87,6 +102,7 @@ export default function CreateScenario({ scenarios }) {
       expenseRatio: 0,
       expectedAnnualIncome: 0,
       taxability: false,
+      random: [0, 0, 0, 0, 0, 0]
     };
     const investmentTypeResponse = await createInvestmentType(newInvestmentType);
     const createdInvestmentType = investmentTypeResponse.data; // <-- now you have _id
@@ -104,6 +120,8 @@ export default function CreateScenario({ scenarios }) {
     const newAllocation = {
       investment: newInvestment,
       percentage: 0,
+      finalPercentage: 0,
+      glide: 0
     };
   
     const newInvestEvent = {
@@ -112,8 +130,10 @@ export default function CreateScenario({ scenarios }) {
       description: "",
       startYear: new Date().getFullYear(),
       duration: 1,
+      random: [0, 0, 0, 0, 0, 0],
       allocations: [newAllocation],
       max: 0,
+      glide: false
     };
     const eventResponse = await eventsApi.createEvent(newInvestEvent);
     const createdEvent = eventResponse.data;
@@ -142,6 +162,7 @@ export default function CreateScenario({ scenarios }) {
       sharing: formData.sharing,
       financialGoal: Number(formData.financialGoal),
       state: formData.state,
+      random: formData.random,
     };
   
     console.log("Sending scenario:", newScenario);
@@ -177,35 +198,56 @@ export default function CreateScenario({ scenarios }) {
   ];
 
   return (
-    <div id="add_event">
-      <form onSubmit={submit}>
-        <InputField id="name" type="text" value={formData.name} onChange={handleInputChange}>Scenario Name</InputField>
+        <div id = "add_event">
+            <form onSubmit={submit}>
+                <InputField id="name" type="text" value={formData.name} onChange={handleInputChange}>Scenario Name</InputField>
+                
+                <label htmlFor="married">Marital Status*</label>
+                <input type="radio" name="married" value={false} onChange={handleRadioChange} required/> Single
+                <input type="radio" name="married" value={true} onChange={handleRadioChange} /> Married
 
-        <label htmlFor="married">Marital Status*</label>
-        <input type="radio" name="married" value={false} onChange={handleRadioChange} required /> Single
-        <input type="radio" name="married" value={true} onChange={handleRadioChange} /> Married
+                <InputField id="birthYearUser" type="number" value={formData.birthYearUser} onChange={handleInputChange}>Birth Year</InputField>
+                {formData.random[0] === 0 && <InputField id="lifeExpectancyUser" type="number" value={formData.lifeExpectancyUser} onChange={handleInputChange}>Life Expectancy (Years)</InputField>}
+                <div style={{ display: 'flex', gap: '10px'}}>
+                    <label htmlFor="random_life">Life Expectancy Sampling</label>
+                    <input type="checkbox" id="random_life" name="random_life" value={formData.random[0] === 0} onChange={handleRandom} style={{ marginBottom: '15px' }}/>
+                </div>
+                {formData.random[0] !== 0 && <div style={{ display: 'flex', alignItems: 'center', gap: '20px' }}>
+                    <label htmlFor="mean" style={{ marginBottom: '20px' }}>Mean</label>
+                    <input type="number" name="mean" value={formData.random[1]} onChange={handleRandom} required />
+                    <label htmlFor="sd" style={{ marginBottom: '20px' }}>Standard Deviation</label>
+                    <input type="number" name="sd" value={formData.random[2]} onChange={handleRandom} required />
+                </div>}
 
-        <InputField id="birthYearUser" type="number" value={formData.birthYearUser} onChange={handleInputChange}>Birth Year</InputField>
-        <InputField id="lifeExpectancyUser" type="number" value={formData.lifeExpectancyUser} onChange={handleInputChange}>Life Expectancy (Years)</InputField>
+                {formData.married === "true" && <InputField id="birthYearSpouse" type="number" value={formData.birthYearSpouse} onChange={handleInputChange}>Spouse Birth Year</InputField>}
+                {formData.married === "true" && formData.random[0] === 0 && <InputField id="lifeExpectancySpouse" type="number" value={formData.lifeExpectancySpouse} onChange={handleInputChange}>Spouse Life Expectancy (Years)</InputField>}
 
-        {formData.married === "true" && <InputField id="birthYearSpouse" type="number" value={formData.birthYearSpouse} onChange={handleInputChange}>Spouse Birth Year</InputField>}
-        {formData.married === "true" && <InputField id="lifeExpectancySpouse" type="number" value={formData.lifeExpectancySpouse} onChange={handleInputChange}>Spouse Life Expectancy (Years)</InputField>}
+                {formData.random[3] === 0 && <InputField id="inflation" type="number" value={formData.inflation} onChange={handleInputChange}>Inflation (%)</InputField>}
+                <div style={{ display: 'flex', gap: '10px'}}>
+                    <label htmlFor="random_inflation">Inflation Sampling</label>
+                    <input type="checkbox" id="random_inflation" name="random_inflation" value={formData.random[3] === 0} onChange={handleRandom} style={{ marginBottom: '15px' }}/>
+                </div>
+                {formData.random[3] !== 0 && <div style={{ display: 'flex', alignItems: 'center', gap: '20px' }}>
+                    <label htmlFor="mean_inflation" style={{ marginBottom: '20px' }}>Mean</label>
+                    <input type="number" name="mean_inflation" value={formData.random[4]} onChange={handleRandom} required />
+                    <label htmlFor="sd_inflation" style={{ marginBottom: '20px' }}>Standard Deviation</label>
+                    <input type="number" name="sd_inflation" value={formData.random[5]} onChange={handleRandom} required />
+                </div>}
 
-        <InputField id="inflation" type="number" value={formData.inflation} onChange={handleInputChange}>Inflation (%)</InputField>
-        <InputField id="annualLimit" type="number" value={formData.annualLimit} onChange={handleInputChange}>Annual Contribution Limit ($)</InputField>
+                <InputField id="annualLimit" type="number" value={formData.annualLimit} onChange={handleInputChange}>Annual Contribution Limit ($)</InputField>
 
-        <InputField id="rothOptimizer" type="checkbox" checked={formData.rothOptimizer} onChange={handleInputChange}>Roth Optimizer</InputField>
-        <InputField id="financialGoal" type="number" value={formData.financialGoal} onChange={handleInputChange}>Financial Goal ($)</InputField>
+                <InputField id="rothOptimizer" type="checkbox" checked={formData.rothOptimizer} onChange={handleInputChange}>Roth Optimizer</InputField>
+                <InputField id="financialGoal" type="number" value={formData.financialGoal} onChange={handleInputChange}>Financial Goal ($)</InputField>
 
-        <label htmlFor="state">State of Residence</label>
-        <select id="state" name="state" value={formData.state} onChange={handleInputChange} required>
-          <option value="">--Select a State--</option>
-          {states.map((state, index) => (<option key={index} value={state}>{state}</option>))}
-        </select>
+                <label htmlFor="state">State of Residence</label>
+                <select id="state" name="state" value={formData.state} onChange={handleInputChange} required>
+                    <option value="">--Select a State--</option>
+                    {states.map((state, index) => (<option key={index} value={state}>{state}</option>))}
+                </select>
 
-        <button type="submit">Submit</button>
-        {error && <div className="error">{error}</div>}
-      </form>
-    </div>
-  );
+                <button type="submit">Submit</button>
+                {error && <div className="error">{error}</div>}
+            </form>
+        </div>      
+    )
 }
